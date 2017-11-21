ab477a2308ac7ebc02bfb8aa713d4646d79ddc61
cyz <cyz@111.com>
2017-11-21 18:50:59 +0800

    完善登陆功能

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,11 @@
 /**
  * Created by Administrator on 2017/11/21.
  */
+
+
+$(document).ajaxStart(function () {
+  NProgress.start();
+});
+$(document).ajaxStop(function () {
+  NProgress.done();
+});
