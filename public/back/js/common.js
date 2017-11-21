08950741ff8084d76690404f9782695263fdbdea
cyz <cyz@111.com>
2017-11-21 19:07:32 +0800

    登陆功能改善

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -2,10 +2,15 @@
  * Created by Administrator on 2017/11/21.
  */
 
+NProgress.configure({
+  showSpinner: false
+	});
 
 $(document).ajaxStart(function () {
   NProgress.start();
 });
 $(document).ajaxStop(function () {
-  NProgress.done();
+  setTimeout(function () {
+    NProgress.done();
+  }, 500);
 });
